389290010effa330acbddf1e714977f1542a867c
cs97 <chrswr97@gmail.com>
2024-03-07 14:56:24 +0100

    Update formular.js

--- formular.js
+++ formular.js
@@ -49,12 +49,6 @@ function senden() {
     // Update Post 
     http.put('http://85.215.154.152:8080/submit', data) 
       
-    // Resolving promise for response data 
-    .then(data => console.log(data)) 
-      
-    // Resolving promise for error 
-    .catch(err => console.log(err));
-    
         
 }
 
