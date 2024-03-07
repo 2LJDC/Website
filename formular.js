6f43961df251483605d67e5080976b8ce85725dd
cs97 <chrswr97@gmail.com>
2024-03-07 14:50:05 +0100

    Update formular.js

--- formular.js
+++ formular.js
@@ -35,18 +35,27 @@ function senden() {
     const kunde = new Kunde (anrede, name, geburtsdatum, mail, tel, vorlage, farbe, eigeneVorstellungen, sonstiges);
     alert(kunde.toString());
 
-    const http = new EasyHTTP();
-    const output = JSON.stringify(kunde);
 
+    
+    // Instantiating new EasyHTTP class 
+    const http = new EasyHTTP; 
+    // User Data 
     const data = { 
-      name: 'Hakuna Matata', 
-      username: 'Simba', 
-      email: 'simba@gmail.com'
-    } 
-  
+        name: 'Hakuna Matata', 
+        username: 'Simba', 
+        email: 'simba@gmail.com'
+      } 
+      
+    // Update Post 
     http.put('http://85.215.154.152:8080/submit', data) 
+      
+    // Resolving promise for response data 
     .then(data => console.log(data)) 
+      
+    // Resolving promise for error 
     .catch(err => console.log(err));
+    
+        
 }
 
 class Kunde{
